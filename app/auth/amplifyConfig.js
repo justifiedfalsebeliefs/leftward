2cca69ef21aa4d3ea8dfa80a7ad9d69b6c815217
Luke Pendergrass <lrpendergrass@gmail.com>
2020-11-24 17:47:29 -0800

    Add amplify config file

---
+++ app/auth/amplifyConfig.js
@@ -0,0 +1,5 @@
+export default { Auth: {
+        region: '', //  Amazon Cognito Region
+        userPoolId: '', // Amazon Cognito User Pool ID
+        userPoolWebClientId: '', // app client ID
+    }}
