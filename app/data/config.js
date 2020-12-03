c2a64bce1d7bcf68da4646b39669e8381f0f039e
Luke Pendergrass <lrpendergrass@gmail.com>
2020-12-02 22:28:38 -0800

    update to reflect dev docker environment server

--- app/data/config.js
+++ app/data/config.js
@@ -1,4 +1,4 @@
 export default function getEndpoint(endpoint) {
-    const slug = "http://45.79.90.235/" //"http://10.0.2.2:5000/" 
+    const slug = "http://10.0.2.2:80/"
     return slug.concat(endpoint)
 }
